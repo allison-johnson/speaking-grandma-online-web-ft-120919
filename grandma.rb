4143d55333189f6af1115165ada170a5cebf4e50
Allison Johnson <allison.cuttler@gmail.com>
2019-12-09 15:21:04 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 
 def speak_to_grandma(phrase)
   if(phrase.upcase == phrase)
-    if(phrase.upcase == "I LOVE YOU GRANDMA!")
+    if(phrase == "I LOVE YOU GRANDMA!")
       "I LOVE YOU TOO PUMPKIN!"
     end #inner if
     "NO, NOT SINCE 1938!"
